b26fbf77eb46052d85ab3ef9ce59025869ab23f4
Johan Garzon <johanprograma@hotmail.com>
2020-08-17 15:15:58 +0200

    Remove resource works

--- index.js
+++ index.js
@@ -11,9 +11,34 @@ db.loadDatabase(function (err) {    // Callback is optional
 
 app.post('/create', (req, res) => {
   db.insert(req.body, function() {
-    console.log(req.body)
     res.send('Done').status(200);
   })
+})
+
+app.put('/update/:id', (req, res) =>  {
+  const query = {
+    _id: req.params.id
+  }
+  db.update(query, req.body, {multi: true}, (err) => { 
+    if(err) {
+      res.send('ID not found').status(400)
+    } else {
+      res.send('Updated').status(200)
+    }
+  }); 
+})
+
+app.delete('/delete/:id', (req, res) => {
+  const query = {
+    _id: req.params.id
+  }
+  db.remove(query, {multi: true}, (err) => { 
+    if(err) {
+      res.send('ID not found').status(400)
+    } else {
+      res.send('Removed').status(200)
+    }
+  }); 
 })
 
 app.listen(3000, () => { console.log('App listening on port 3000')})
