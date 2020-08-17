8185acec9ac8015612c750c3d0d6e852175e7b60
Johan Garzon <johanprograma@hotmail.com>
2020-08-17 14:58:24 +0200

    Insert resource via /insert

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: './database.db' });
+db.loadDatabase(function (err) {    // Callback is optional
+  // Now commands will be executed
+});
 
 app.post('/create', (req, res) => {
-  console.log(req.body)
+  db.insert(req.body, function() {
+    console.log(req.body)
+    res.send('Done').status(200);
+  })
 })
 
 app.listen(3000, () => { console.log('App listening on port 3000')})
